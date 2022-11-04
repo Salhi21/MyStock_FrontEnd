330840d4397d81803ba7760d778f56f1fa863d02
Salhi Nassim <salhi.nassim.21@gmail.com>
2022-11-04 09:21:24 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Gestion_des_produits_pharmaceutique';
+}
